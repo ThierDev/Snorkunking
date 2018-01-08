68cdb711a98f07407d8c9badcaed135cea7ebab1
VictorLebrun <victorhidole@gmail.com>
2018-01-08 17:07:53 +0100

    Spawn coffre random beta

--- NiveauC.java
+++ NiveauC.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class NiveauC extends Main{
@@ -6,6 +8,7 @@ public class NiveauC extends Main{
     public int totalNiveau;
     public double NiveauHeight;
     public Coffre coffreN;
+    Random randomGenerator = new Random();
     
     public NiveauC(int typeNiveau,int position,int totalNiveau){ 
         if (typeNiveau==0){
@@ -33,10 +36,11 @@ public class NiveauC extends Main{
         return (2*NiveauHeight +((totalNiveau-position)*NiveauHeight));
     }
     public void drawNiveau(){
+    	int X_Coffre = 64 + randomGenerator.nextInt(508-1);
         setPenColors(ColorArg);
         double Y=positionYCenterNiveau();
-        StdDraw.rectangle(X_MAX/2,Y*SH,X_MAX/2-0.1*X_MAX,NiveauHeight*SH);
-        coffreN.showCoffre(X_MAX/2,(Y-NiveauHeight/2)*SH,10*SW,NiveauHeight*SH);
+        StdDraw.rectangle(X_MAX/2,Y*SH,0.4*X_MAX,NiveauHeight*SH);
+        coffreN.showCoffre(X_Coffre,(Y-NiveauHeight/2)*SH,10*SW,NiveauHeight*SH);
     }
     
     public void setPenColors(String argString){
@@ -47,6 +51,7 @@ public class NiveauC extends Main{
         else if(argString.equals("Green")){StdDraw.setPenColor(StdDraw.GREEN);}
         else {StdDraw.setPenColor();}
     }
+    
     public int getTresor(){
         return coffreN.getTresor();
     }
